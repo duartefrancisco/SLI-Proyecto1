bbae50c0f4baded1d1a86c81a9f019aaf74a1395
Francisco Duarte <francisco@duosys.gt>
2022-05-29 14:50:54 -0600

    Se agrega paso 5

--- helpers/RegresionLineal.py
+++ helpers/RegresionLineal.py
@@ -7,12 +7,13 @@ import sklearn as sl
 
 class RegresionLineal:
 
-    def __init__(self, dataset, trainSize = 0.8):
+    def __init__(self, dataset, target, trainSize = 0.8):
         self.datasetOriginal = dataset.copy()
         
         limiteParticion = int(len(dataset) * trainSize)
         self.datasetTrain = dataset[0 : limiteParticion, :]
         self.datasetTest = dataset[limiteParticion : , : ]
+        self.target = target
 
     def AnalisisExploratorio(self):
         self.datasetOriginal.describe()
@@ -20,3 +21,14 @@ class RegresionLineal:
     def GenerarHistogramas(self):
         for columna in self.datasetOriginal.columns:
             sns.displot(self.datasetOriginal[columna])
+
+    def AnalisisVariablesIndependientes(self):
+        for columna in self.datasetTrain.columns:
+            if columna != self.target:
+                correlacion = self.datasetTrain[self.target].corr(self.datasetTrain[columna], method = "pearson")
+                #plt.figure(figsize = (15,6))
+                plt.xlabel(columna)
+                plt.ylabel(self.target)
+                plt.title(f"{columna} vs {self.target} - Correlación: {correlacion}")
+                plt.scatter(self.datasetTrain[columna], self.datasetTrain[self.target])
+                plt.show()
